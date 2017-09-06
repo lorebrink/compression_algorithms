f54a416179e37ef690a96d80efd80773f6cd0593
lukas lorentzon <lukas.lorentzon@gmail.com>
2017-09-06 22:56:43 +0200

    A crude makefile and a very simple base test sample for huffman tests.

---
+++ unittests/huffman/huffman_test.cc
@@ -0,0 +1,25 @@
+/*
+ * huffman_test.cc
+ *
+ *  Created on: Sep 6, 2017
+ *      Author: lukas
+ */
+
+// Inlude googletest and googlemock
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+extern "C"
+{
+  #include "huffman.h"
+}
+
+#include <string>
+
+TEST(HuffmanTest, DummyTest) {
+  std::string file_path = "test.txt";
+  const std::string file_path_result = CompressFile(file_path.c_str());
+  ASSERT_EQ(file_path, file_path_result);
+}
+
+
